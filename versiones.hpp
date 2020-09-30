293251d6b3e9665fb6c911098a3ba61892d924d7
Ju0w1 <pablopeculio@cras-dev.com>
2020-09-30 19:23:20 -0300

    Update versiones.hpp

--- versiones.hpp
+++ versiones.hpp
@@ -1,10 +1,23 @@
 #ifndef VERSIONES_H
 #define VERSIONES_H
 
+enum retorno{
+	OK, ERROR, NO_IMPLEMENTADA
+};
+typedef enum retorno TipoRet;
 
+struct archivo{
+	
+};
+typedef archivo* Archivo;
 
+struct cadena{
+	
+};
 
 void menu();
+
+
 
 
 #endif
